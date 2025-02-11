9b2030f7f2c4aa53309cac071062caf7e3b9f08a
PePorMAty <themranti3256@gmail.com>
2025-02-11 18:57:58 +0700

    move Navbar to src/components

--- src/pages/Comparison/Comparison.tsx
+++ src/pages/Comparison/Comparison.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 
-import { Navbar } from 'components/Layout/components/Navbar';
+import { Navbar } from 'components/Navbar';
 
 export const Comparison = () => {
   return (
